5a410dc000d514e86635519f4b1cff6436d9a38a
wakametarou <wakame.programing@gmail.com>
2022-02-12 22:43:10 +0900

    追加テーブル作成用処理作成準備

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   private
 
     def sign_up_params
-      params.permit(:email, :password, :password_confirmation, :name, :patient_or_doctor, :sex)
+      params.permit(
+        :email, :password, :password_confirmation, :name, :patient_or_doctor, :sex,
+        patient_info_attributes: [:room_number, :phone_number, :emergency_address, :address, :bilding]
+      )
     end
 end
