04d02edc8a500b3d97fb497bf7082eaefb4fe9c3
wakametarou <wakame.programing@gmail.com>
2022-02-24 05:38:04 +0900

    user/index,showの処理実装完了 apiも設計済み #29

--- backend/test/controllers/api/v1/users_controller_test.rb
+++ backend/test/controllers/api/v1/users_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_users_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get api_v1_users_show_url
     assert_response :success
   end
