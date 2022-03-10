82cd08fccee1fd6a58df05ab68d81676b3862549
wakametarou <wakame.programing@gmail.com>
2022-03-11 08:00:28 +0900

    users apiテスト完了 #73

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe 'UsersAPI' do
-    context "一覧の表示" do
-      it "患者様の一覧の表示" do
-        FactoryBot.create(:user)
+    context "患者様情報の表示" do
+      it "患者様一覧の表示" do
+        user = FactoryBot.create(:user)
         doctor = FactoryBot.create(:doctor)
         auth_tokens = sign_in(doctor)
         get "/api/v1/users/index", headers: auth_tokens
+        expect(response).to have_http_status(200)
+      end
+
+      it "患者様詳細の表示" do
+        user = FactoryBot.create(:user)
+        doctor = FactoryBot.create(:doctor)
+        auth_tokens = sign_in(doctor)
+        get "/api/v1/users/show/?id=#{user.id}", headers: auth_tokens
         expect(response).to have_http_status(200)
       end
     end
