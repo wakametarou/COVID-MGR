404fc1a91619b2cf22bab937984961f049ae42d5
wakametarou <wakame.programing@gmail.com>
2022-03-12 07:13:29 +0900

    other model 単体テスト完了 #76

--- backend/spec/models/other_symptom_spec.rb
+++ backend/spec/models/other_symptom_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe OtherSymptom, type: :model do
       end
     end
   end
+
+  describe '入力項目の有無' do
+    let(:new_other){OtherSymptom.new}
+    context 'db保存時に必須入力であること' do
+      it '痛みの程度が必須であること' do
+        expect(new_other).not_to be_valid
+        expect(new_other.errors[:pain_degree]).to include(I18n.t('errors.messages.blank'))
+      end
+
+      it '症状の詳細が必須であること' do
+        expect(new_other).not_to be_valid
+        expect(new_other.errors[:concrete]).to include(I18n.t('errors.messages.blank'))
+      end
+
+      it '問診idが必須であること' do
+        expect(new_other).not_to be_valid
+        expect(new_other.errors[:interview_id]).to include(I18n.t('errors.messages.blank'))
+      end
+
+      it '保存できないこと' do
+        expect(new_other.save).to be_falsey
+      end
+    end
+  end
 end
