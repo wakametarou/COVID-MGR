03f177acc839ce737bd3e12b81b8650ea1545f58
wakametarou <wakame.programing@gmail.com>
2022-03-12 07:10:58 +0900

    other_symptom model 保存テスト完了 #76

--- backend/spec/models/other_symptom_spec.rb
+++ backend/spec/models/other_symptom_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe OtherSymptom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '問診作成機能' do
+    context 'その他症状作成' do
+      it '正しく作成出来る事'do
+        user = FactoryBot.create(:user)
+        interview = FactoryBot.create(:interview, user_id: user.id)
+        other = OtherSymptom.new(pain_degree: 1, concrete: "痛い", interview_id: interview.id)
+        expect(other).to be_valid
+        other.save
+        answered_other = OtherSymptom.find_by(interview_id: interview.id)
+        expect(answered_other.pain_degree).to eq(1)
+        expect(answered_other.concrete).to eq("痛い")
+        expect(answered_other.interview_id).to eq(interview.id)
+      end
+    end
+  end
 end
