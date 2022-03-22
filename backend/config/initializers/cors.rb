41f1401d89a52c2b5ae3d7b322e840dae96d7321
wakametarou <wakame.programing@gmail.com>
2022-03-22 22:16:47 +0900

    本番分岐 #103

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', 'http://covid-mgr.com', 'https://covid-mgr.com'
 
     resource '*',
              headers: :any,
              expose: %w[access-token expiry token-type uid client],
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
